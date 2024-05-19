1df4cee3aa128dfe92b48b0f115be198e615cdd2
Dara <106348154+prince-of-maroc@users.noreply.github.com>
2024-05-19 19:36:25 -0400

    Set vim options

---
+++ options.lua
@@ -0,0 +1,13 @@
+local options = {
+	number = true,
+	relativenumber = true,
+	mouse = "",
+	mousehide = true,
+	autoindent = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	pumheight = 6,
+	linebreak = true
+}
+
+for k,v in pairs(options) do vim.opt[k] = v end
