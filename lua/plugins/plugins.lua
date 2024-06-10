9062ed285fea3986291ccd4b5e8d5398d8d7286b
Dara <106348154+prince-of-maroc@users.noreply.github.com>
2024-06-10 15:21:05 -0400

    Delete foreign comments from documentation

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -11,7 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", 
     lazypath,
   })
 end
@@ -25,10 +25,10 @@ require("lazy").setup({
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"nvim-tree/nvim-web-devicons", 
 			"MunifTanjim/nui.nvim",
-			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-			}
+			"3rd/image.nvim",
+		}
 	},
 	{ 
 		-- Toggleterm
