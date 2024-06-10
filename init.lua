5518311ab5793717e176f9d2eddae3a5be0f3136
Dara <106348154+prince-of-maroc@users.noreply.github.com>
2024-06-10 15:38:34 -0400

    Highlight cursor row number

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ require "keymaps"
 require "plugins/plugins"
 
 vim.cmd("colorscheme darkplus")
+
+-- Set line number colorscheme
+vim.cmd "hi CursorLineNR guifg=white"
+vim.cmd "set cursorline"
+vim.cmd "set cursorlineopt=number"
