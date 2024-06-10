45f7ae640f4acfe10a5a31aa40dba12a88da7e1b
Dara <106348154+prince-of-maroc@users.noreply.github.com>
2024-06-10 14:57:14 -0400

    Add lualine plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -11,6 +11,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+
+
+
+
+-- PLUGIN INSTALLATION 
+
 require("lazy").setup({
 	{
 		"nvim-neo-tree/neo-tree.nvim",
@@ -32,11 +38,16 @@ require("lazy").setup({
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = true
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
+	},
+	{
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
 	}
-		
 })
+
+
+
+
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
@@ -44,6 +55,12 @@ vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+
+
+
+
+
+-- PLUGIN CONFIGURATIONS
 
 
 -- neo-tree config
@@ -62,3 +79,44 @@ require("toggleterm").setup({
 		open_mapping = [[<c-\>]],
 		close_on_exit = true,
 })
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
