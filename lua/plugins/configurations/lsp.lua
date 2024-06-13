c0eb7250521efc623ab0d025b790194b78b57346
Dara <106348154+prince-of-maroc@users.noreply.github.com>
2024-06-12 21:19:09 -0400

    Modify LSP keymapping

--- lua/plugins/configurations/lsp.lua
+++ lua/plugins/configurations/lsp.lua
@@ -14,10 +14,10 @@ lsp.ensure_installed({
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<Down>'] = cmp.mapping.select_next_item(cmp_select),
-	['<Tab>'] = cmp.mapping.confirm({select = true}),
-	['<C-Space>'] = cmp.mapping.complete(),
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-Space>'] = cmp.mapping.confirm({select = true}),
+	['<C-y>'] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
